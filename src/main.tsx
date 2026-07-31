50ba53acce9085c422218e66f3445254316b1d6f
floortjevanoosterom-gif <floortjevanoosterom@hotmail.com>
2026-07-31 15:11:28 +0200

    Fix App import path

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+```tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './pages/App.tsx';
+import './pages/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+```
